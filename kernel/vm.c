bcd7fe2dcf9d83ad52f8cfcbf3dc71dcd54eed0a
blindinlights <blindinlightscc@outlook.com>
2024-04-08 16:55:36 +0800

    vmprint

--- kernel/vm.c
+++ kernel/vm.c
@@ -432,3 +432,25 @@ copyinstr(pagetable_t pagetable, char *dst, uint64 srcva, uint64 max)
     return -1;
   }
 }
+void vmprint_helper(pagetable_t pagetable,int depth){
+  int flag=0;
+  
+  for(int i = 0; i < 512; i++){
+    pte_t pte = pagetable[i];
+    if(pte & PTE_V){
+
+    for(int i=0;i<depth;i++){
+        printf(" ..");
+    }      
+      uint64 child = PTE2PA(pte);
+      printf("%d: pte %p pa %p\n",i,pte,child);
+      if((pte&(PTE_R|PTE_X|PTE_W))==0)
+      vmprint_helper((pagetable_t)child,depth+1 );
+    } 
+  }
+}
+void vmprint(pagetable_t pagetable){
+  printf("page table %p\n",pagetable);
+  vmprint_helper(pagetable,1);
+}
+
